bb3879fcc64b78b11425661a755acd39ee850454
Sergey Moskvitin <moskvitin.sergei@yandex.ru>
2020-09-27 13:46:31 +0300

    refactor: правка скрипта и отображения

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const buttenOpen = document.querySelector(".profile__edit-button");
 const buttenClose = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
-const submitButton = document.querySelector(".popup__button");
+const submitButton = document.querySelector(".popup__content");
 let name = document.querySelector(".profile__name");
 let about = document.querySelector(".profile__about");
 let nameNew = document.querySelector(".popup__name");
@@ -9,16 +9,20 @@ let aboutNew = document.querySelector(".popup__about");
 
 function popupToggle() {
   popup.classList.toggle("popup_is-opened");
+
   nameNew.value = name.textContent;
   aboutNew.value = about.textContent;
 }
 
 function saveData(event) {
+  event.preventDefault();
+
   name.textContent = nameNew.value;
   about.textContent = aboutNew.value;
+
   popup.classList.toggle("popup_is-opened");
 }
 
 buttenOpen.addEventListener("click", popupToggle);
 buttenClose.addEventListener("click", popupToggle);
-submitButton.addEventListener("click", saveData);
+submitButton.addEventListener("submit", saveData);
