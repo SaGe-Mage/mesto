582760130f3179eaf2c5b390ba93e34c3df4af9c
Sergey Moskvitin <moskvitin.sergei@yandex.ru>
2020-10-08 15:02:27 +0300

    build: увеличение картинок

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,10 @@ const buttenOpenEdit = document.querySelector('.profile__edit-button');
 const buttenOpenAdd = document.querySelector('.profile__add-button');
 const buttenCloseEdit = document.querySelector('.popup__close-button_place_edit');
 const buttenCloseAdd = document.querySelector('.popup__close-button_place_add');
+const buttenClosePic = document.querySelector('.popup__close-button_place_pic');
 const popupEdit = document.querySelector('.popup_place_edit');
 const popupAdd = document.querySelector('.popup_place_add');
+const popupPic = document.querySelector('.popup_place_pic');
 const submitButtonEdit = document.querySelector('.popup__content_place_edit');
 const submitButtonAdd = document.querySelector('.popup__content_place_add');
 const gallery = document.querySelector('.gallery');
@@ -12,36 +14,35 @@ let about = document.querySelector('.profile__about');
 let nameNew = document.querySelector('.popup__input_place_name');
 let aboutNew = document.querySelector('.popup__input_place_about');
 
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
 function popupOpen(popup) {
   popup.classList.add('popup_is-opened');
-  
+
   if (popup.classList.contains('popup_place_edit')) {
     nameNew.value = name.textContent;
     aboutNew.value = about.textContent;
@@ -72,6 +73,16 @@ function addCard(name, link) {
     evt.target.parentElement.remove();
   });
 
+  element.querySelector('.element__pic').addEventListener('click', evt => {
+    let pic = popupPic.querySelector('.popup__pic');
+    let capture = popupPic.querySelector('.popup_caption');
+
+    pic.setAttribute('src', evt.target.src);
+    capture.textContent = evt.target.nextElementSibling.firstElementChild.textContent;
+
+    popupPic.classList.add('popup_is-opened');
+  });
+
   element.querySelector('.element__like').addEventListener('click', evt => {
     evt.target.classList.toggle('element__like_active');
   });
@@ -86,21 +97,22 @@ function newCard(event) {
   let link = document.querySelector('.popup__input_place_link');
 
   addCard(location.value, link.value);
-  
+
   location.value = '';
   link.value = '';
-  
+
   popupClose(popupAdd);
 }
 
 buttenOpenEdit.addEventListener('click', () => popupOpen(popupEdit));
 buttenOpenAdd.addEventListener('click', () => popupOpen(popupAdd));
-buttenCloseEdit.addEventListener('click',() => popupClose(popupEdit));
-buttenCloseAdd.addEventListener('click',() => popupClose(popupAdd));
+buttenCloseEdit.addEventListener('click', () => popupClose(popupEdit));
+buttenCloseAdd.addEventListener('click', () => popupClose(popupAdd));
+buttenClosePic.addEventListener('click', () => popupClose(popupPic));
 submitButtonEdit.addEventListener('submit', saveData);
 submitButtonAdd.addEventListener('submit', newCard);
 
 
-initialCards.forEach(function(item) {
+initialCards.forEach(function (item) {
   addCard(item.name, item.link);
 });
